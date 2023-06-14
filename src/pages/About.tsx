04b9ae6162a886a9fa1334242f30117f928889ce
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2023-06-14 14:12:44 -0700

    Updated content

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,9 +6,12 @@ const About = () => {
       >
         Hi, I'm Bitian.
       </h1>
-      <p>
+      <p className="my-5">
         I'm a software engineer and I enjoy cooking, hiking, hosting boardgame nights, and playing
         music with friends and family.
+      </p>
+      <p>
+        Technologies used for this app: React, Bootstrap, TypeScript, and Vite.
       </p>
       <p className="mt-5 fs-4">
         You can find me at <a href="https://github.com/bzzz-coding">GitHub</a>, <a href="https://twitter.com/Bzzz_coding">Twitter</a>, and <a href="https://www.linkedin.com/in/bitian-zhang/">LinkedIn</a>, or reach me at bzzz.coding@gmail.com 
